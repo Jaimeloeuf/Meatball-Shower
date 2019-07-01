fd03dea20f251fbdd6c171f1ba89715f91a9a86c
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-07-02 00:43:01 +0800

    Updated sketch.js: 1.  Created a few game wide global variables for keeping track of things     like current image prediction and more.
    2.  Updated sketch to use the startPrediction function from the ai module.
    
    3.  Created functions to group actions to be done when the game modes /
        state changes from one to another. Like for example functions to show
        and hide buttons and more.

--- sketch.js
+++ sketch.js
@@ -1,12 +1,22 @@
 // Webcam input and regression model
 let video;
 let regressor;
+let currentPrediction = 0;
+
+let gameState = 0;
 
 // Buttons
 let leftButton, centerButton, rightButton, trainButton;
 
+// Game variables
+let dodged;
+let hiScore = 0;
+let sauce, meatballs, stars;
+
+
 function setup() {
     // Create a p5.js canvas and inject it into the canvas container in the HTML
+    // @Todo  Update the canvas size to be dynamic and injected externally
     const canvas = createCanvas(640, 480);
     background(0);
     noStroke(30);
@@ -14,6 +24,7 @@ function setup() {
 
 
     // Create a video stream using the attached device camera. Will cause a popup asking for Camera permission
+    // const video = createCapture(VIDEO, () => console.log('Video ready'));
     video = createCapture(VIDEO, () => console.log('Video ready'));
     video.hide();
 
@@ -46,14 +57,43 @@ function setup() {
             // If the value is null, it means that training is complete
             if (loss === null) {
                 console.log("Training done");
+                startPrediction();
 
-                /* Create a continuous prediction loop */
-
-                /* Start game */
+                // Start game
+                changeState(1);
             }
 
             // Log the output value from training the model
             // console.log(loss);
         });
     });
+}
+
+
+// Function to show all the buttons
+function showBtns() {
+    select('#info').html('Add images you wish to correspond to relevant movements. Press train when done.');
+    select('#buttons').show();
+    select('#train').show();
+}
+
+// Remove all the items before the game start
+function hideBtns() {
+    select('#buttons').hide();
+    select('#train').hide();
+}
+
+// Function to start the game
+function startGame() {
+    // When the state is newly changed to start game, create the stuff needed for the game
+    dodged = 0;
+    sauce = new Sauce();
+    meatballs = [new Meatball()];
+    makeStars();
+    select('#info').html('0 meatballs dodged. High score: ' + hiScore);
+}
+
+// Function to game over screen
+function endGame() {
+    select('#info').html('Press space to restart');
 }
