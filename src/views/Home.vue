b784af650b48ffd6b262c9d3f7b5a4fa50a048ab
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-07-24 17:52:19 +0800

    Built super basic video out component in "Home":     Using p5.js library and the p5.dom library, a super basic component     that takes in a video stream from the user's default camera input     and draws it as a mirror image on a p5.js canvas.     This will be the foundation upon which the game will be built on.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,35 @@
 <template>
   <div class="home">
+    <div id="canvas"></div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'home'
+import p5 from "p5";
+import { VIDEO } from "p5/lib/addons/p5.dom";
+
+// Variable referencing the video capture stream
+var video;
+
+function setup() {
+  createCanvas(640, 480);
+
+  video = createCapture(VIDEO, () => console.log("Video ready"));
+  video.hide();
 }
+function draw() {
+  // Show mirror image of webcam
+  translate(width, 0);
+  scale(-1.0, 1.0);
+
+  // Draw image following dimension of the canvas
+  image(video, 0, 0, width, height);
+}
+
+window.setup = setup;
+window.draw = draw;
+
+export default {
+  name: "home"
+};
 </script>
