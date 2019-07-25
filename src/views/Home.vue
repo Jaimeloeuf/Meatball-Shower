39c55a34ac1ce3e890b9a9fb510a9aa8e364550d
Jaime Loeuf <jaimeloeuf@gmail.com>
2019-07-25 19:29:54 +0800

    Updated Home view: 1.  Added html template into the component
    2.  Moved the video out canvas onto the canvas element specified in the
        template, instead of just being injected into random positions by p5.js
    
    3.  Added in the createButton functions that create basic empty buttons
        for a start and called the function in the setup function.
    
    4.  Added a call to the setup() function in the vue component's mounted
        function, so that the setup function is ran everytime the SPA change
        back to this component from another component like the About view.
        This is a temporary bug fix that prevents the video output from
        disappearing after switching back to this component.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="home">
+    <div>
+      <h1>Meatball Shower</h1>
+      <p>
+        <i>You must use your webcam to play this game</i>
+      </p>
+
+      <!-- Information panel. Item injected in using JS -->
+      <h2 id="info"></h2>
+    </div>
+
+    <!-- Canvas container contains the canvas element used by p5.js for drawing. -->
     <div id="canvas"></div>
+
+    <!-- Button container contains all the buttons depending on the state of the game-->
+    <div id="buttons"></div>
+
+    <!-- Train container for the train button -->
+    <div id="train"></div>
   </div>
 </template>
 
@@ -12,10 +29,14 @@ import { VIDEO } from "p5/lib/addons/p5.dom";
 var video;
 
 function setup() {
-  createCanvas(640, 480);
+  const canvas = createCanvas(640, 480);
+  // Put the p5.js drawing canvas into the div with "canvas" as ID
+  canvas.parent("canvas");
 
   video = createCapture(VIDEO, () => console.log("Video ready"));
   video.hide();
+
+  CreateButtons();
 }
 function draw() {
   // Show mirror image of webcam
@@ -26,10 +47,30 @@ function draw() {
   image(video, 0, 0, width, height);
 }
 
+function CreateButtons() {
+  const buttonDiv = select("#buttons");
+
+  let leftButton = createButton("Move left");
+  leftButton.parent(buttonDiv);
+
+  let centerButton = createButton("No movement");
+  centerButton.parent(buttonDiv);
+
+  let rightButton = createButton("Move right");
+  rightButton.parent(buttonDiv);
+
+  let trainButton = createButton("Train");
+  select("#train").child(trainButton);
+}
+
 window.setup = setup;
 window.draw = draw;
 
 export default {
-  name: "home"
+  name: "home",
+  mounted() {
+    // Run p5's setup function after everytime it is mounted again.
+    setup();
+  }
 };
 </script>
